a5126d6fa6e2d57eddfd5d112550236a9b6dbba7
Zac Garby <me@zacgarby.co.uk>
2021-07-16 14:30:42 +0100

    Initial Commit

---
+++ Notebar/NotebarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotebarApp.swift
+//  Notebar
+//
+//  Created by Zac Garby on 16/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct NotebarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
